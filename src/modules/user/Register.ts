f5c68c920375c3fd8b76f72662f0a30f3e013ec0
sjvasconcello <sjvasconcello@gmail.com>
2019-01-21 17:58:38 -0300

    Authorization and middleware

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,19 +1,26 @@
-import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { Resolver, Query, Mutation, Arg, UseMiddleware } from "type-graphql";
 import bcrypt from "bcryptjs";
 import { User } from "../../entity/User";
-import { RegisterInput }from "./register/RegisterInput";
+import { RegisterInput } from "./register/RegisterInput";
+import { isAuth } from "../middleware/isAuth";
+import { logger } from "../middleware/logger";
 
 @Resolver()
 export class RegisterResolver {
+  @UseMiddleware(isAuth, logger)
   @Query(() => String)
   async helloWorld() {
     return "Hello world!";
   }
 
   @Mutation(() => User)
-  async register(
-    @Arg("data") {email, firstName, lastName, password}: RegisterInput,
-  ): Promise<User> {
+  async register(@Arg("data")
+  {
+    email,
+    firstName,
+    lastName,
+    password
+  }: RegisterInput): Promise<User> {
     const hashedPassword = await bcrypt.hash(password, 12);
 
     const user = await User.create({
