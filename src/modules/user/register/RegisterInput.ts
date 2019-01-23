4861901eb8120bc215e32b56b8d6d9065d615c39
sjvasconcello <sjvasconcello@gmail.com>
2019-01-22 23:10:35 -0300

    Logout Resolver

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,21 +1,21 @@
 import { Length, IsEmail } from "class-validator";
 import { Field, InputType } from "type-graphql";
 import { IsEmailAlreadyExist } from "./IsEmailAlreadyExist";
-import { PasswordInput } from "../../shared/PasswordInput";
+import { PasswordMixin } from "../../shared/PasswordInput";
+
 
 @InputType()
-export  class RegisterInput extends PasswordInput{
+export class RegisterInput extends PasswordMixin(class {}) {
   @Field()
-  @Length(1,255)
+  @Length(1, 255)
   firstName: string;
 
   @Field()
-  @Length(1,255)
+  @Length(1, 255)
   lastName: string;
 
   @Field()
   @IsEmail()
   @IsEmailAlreadyExist({ message: "email already in use" })
   email: string;
-
 }
